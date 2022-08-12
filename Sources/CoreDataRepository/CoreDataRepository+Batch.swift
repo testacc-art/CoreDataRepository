970256735ad673317406fd8479977a26d68f9354
Andrew Roan <roanutil@users.noreply.github.com>
2022-08-12 16:14:58 -0500

    Add async overloads for Batch methods
    structured-concurrency

--- Sources/CoreDataRepository/CoreDataRepository+Batch.swift
+++ Sources/CoreDataRepository/CoreDataRepository+Batch.swift
@@ -38,6 +38,19 @@ extension CoreDataRepository {
         }}.eraseToAnyPublisher()
     }
 
+    public func insert(
+        _ request: NSBatchInsertRequest,
+        transactionAuthor: String? = nil
+    ) async -> Result<NSBatchInsertResult, CoreDataRepositoryError> {
+        await context.performInScratchPad { scratchPad in
+            scratchPad.transactionAuthor = transactionAuthor
+            guard let result = try scratchPad.execute(request) as? NSBatchInsertResult else {
+                throw CoreDataRepositoryError.fetchedObjectFailedToCastToExpectedType
+            }
+            return result
+        }
+    }
+
     /// Batch update objects in CoreData
     /// - Parameters
     ///     - _ items: [Model]
@@ -81,6 +94,43 @@ extension CoreDataRepository {
         .eraseToAnyPublisher()
     }
 
+    public func create<Model: UnmanagedModel>(
+        _ items: [Model],
+        transactionAuthor: String? = nil
+    ) async -> (success: [Model], failed: [Model]) {
+        var successes = [Model]()
+        var failures = [Model]()
+        await withTaskGroup(of: _Result<Model, Model>.self, body: { [weak self] group in
+            guard let self = self else {
+                group.cancelAll()
+                return
+            }
+            for item in items {
+                let added = group.addTaskUnlessCancelled {
+                    async let result: Result<Model, CoreDataRepositoryError> = self.create(item, transactionAuthor: transactionAuthor)
+                    switch await result {
+                    case let .success(created):
+                        return _Result<Model, Model>.success(created)
+                    case .failure:
+                        return _Result<Model, Model>.failure(item)
+                    }
+                }
+                if !added {
+                    return
+                }
+            }
+            for await result in group {
+                switch result {
+                case let .success(success):
+                    successes.append(success)
+                case let .failure(failure):
+                    failures.append(failure)
+                }
+            }
+        })
+        return (success: successes, failed: failures)
+    }
+
     /// Batch update objects in CoreData
     /// - Parameters
     ///     - urls: [URL]
@@ -115,6 +165,42 @@ extension CoreDataRepository {
         .eraseToAnyPublisher()
     }
 
+    public func read<Model: UnmanagedModel>(urls: [URL],
+        transactionAuthor: String? = nil
+    ) async -> (success: [Model], failed: [URL]) {
+        var successes = [Model]()
+        var failures = [URL]()
+        await withTaskGroup(of: _Result<Model, URL>.self, body: { [weak self] group in
+            guard let self = self else {
+                group.cancelAll()
+                return
+            }
+            for url in urls {
+                let added = group.addTaskUnlessCancelled {
+                    async let result: Result<Model, CoreDataRepositoryError> = self.read(url)
+                    switch await result {
+                    case let .success(created):
+                        return _Result<Model, URL>.success(created)
+                    case .failure:
+                        return _Result<Model, URL>.failure(url)
+                    }
+                }
+                if !added {
+                    return
+                }
+            }
+            for await result in group {
+                switch result {
+                case let .success(success):
+                    successes.append(success)
+                case let .failure(failure):
+                    failures.append(failure)
+                }
+            }
+        })
+        return (success: successes, failed: failures)
+    }
+
     /// Batch update objects in CoreData
     /// - Parameters
     ///     - _ request: NSBatchInsertRequest
@@ -139,6 +225,19 @@ extension CoreDataRepository {
                 }
             }
         }}.eraseToAnyPublisher()
+    }
+
+    public func update(
+        _ request: NSBatchUpdateRequest,
+        transactionAuthor: String? = nil
+    ) async -> Result<NSBatchUpdateResult, CoreDataRepositoryError> {
+        await context.performInScratchPad { scratchPad in
+            scratchPad.transactionAuthor = transactionAuthor
+            guard let result = try scratchPad.execute(request) as? NSBatchUpdateResult else {
+                throw CoreDataRepositoryError.fetchedObjectFailedToCastToExpectedType
+            }
+            return result
+        }
     }
 
     /// Batch update objects in CoreData
@@ -193,6 +292,46 @@ extension CoreDataRepository {
         .eraseToAnyPublisher()
     }
 
+    public func update<Model: UnmanagedModel>(
+        _ items: [Model],
+        transactionAuthor: String? = nil
+    ) async -> (success: [Model], failed: [Model]) {
+        var successes = [Model]()
+        var failures = [Model]()
+        await withTaskGroup(of: _Result<Model, Model>.self, body: { [weak self] group in
+            guard let self = self else {
+                group.cancelAll()
+                return
+            }
+            for item in items {
+                let added = group.addTaskUnlessCancelled {
+                    guard let url = item.managedRepoUrl else {
+                        return _Result<Model, Model>.failure(item)
+                    }
+                    async let result: Result<Model, CoreDataRepositoryError> = self.update(url, with: item, transactionAuthor: transactionAuthor)
+                    switch await result {
+                    case let .success(created):
+                        return _Result<Model, Model>.success(created)
+                    case .failure:
+                        return _Result<Model, Model>.failure(item)
+                    }
+                }
+                if !added {
+                    return
+                }
+            }
+            for await result in group {
+                switch result {
+                case let .success(success):
+                    successes.append(success)
+                case let .failure(failure):
+                    failures.append(failure)
+                }
+            }
+        })
+        return (success: successes, failed: failures)
+    }
+
     /// Batch delete objects from CoreData
     /// - Parameters
     ///     - _ request: NSBatchInsertRequest
@@ -217,6 +356,19 @@ extension CoreDataRepository {
                 }
             }
         }}.eraseToAnyPublisher()
+    }
+
+    public func delete(
+        _ request: NSBatchDeleteRequest,
+        transactionAuthor: String? = nil
+    ) async -> Result<NSBatchDeleteResult, CoreDataRepositoryError> {
+        await context.performInScratchPad { scratchPad in
+            scratchPad.transactionAuthor = transactionAuthor
+            guard let result = try scratchPad.execute(request) as? NSBatchDeleteResult else {
+                throw CoreDataRepositoryError.fetchedObjectFailedToCastToExpectedType
+            }
+            return result
+        }
     }
 
     /// Batch update objects in CoreData
@@ -257,5 +409,41 @@ extension CoreDataRepository {
             return output
         }
         .eraseToAnyPublisher()
+    }
+
+    public func delete(urls: [URL],
+        transactionAuthor: String? = nil
+    ) async -> (success: [URL], failed: [URL]) {
+        var successes = [URL]()
+        var failures = [URL]()
+        await withTaskGroup(of: _Result<URL, URL>.self, body: { [weak self] group in
+            guard let self = self else {
+                group.cancelAll()
+                return
+            }
+            for url in urls {
+                let added = group.addTaskUnlessCancelled {
+                    async let result: Result<Void, CoreDataRepositoryError> = self.delete(url, transactionAuthor: transactionAuthor)
+                    switch await result {
+                    case .success:
+                        return _Result<URL, URL>.success(url)
+                    case .failure:
+                        return _Result<URL, URL>.failure(url)
+                    }
+                }
+                if !added {
+                    return
+                }
+            }
+            for await result in group {
+                switch result {
+                case let .success(success):
+                    successes.append(success)
+                case let .failure(failure):
+                    failures.append(failure)
+                }
+            }
+        })
+        return (success: successes, failed: failures)
     }
 }
