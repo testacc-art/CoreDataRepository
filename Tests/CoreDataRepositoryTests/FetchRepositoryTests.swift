223369f12adb799cb52fbf36fcb2878aec80624c
testacc-art <74711697+testacc-art@users.noreply.github.com>
2022-08-22 11:46:10 +0200

    Update FetchRepositoryTests.swift

--- Tests/CoreDataRepositoryTests/FetchRepositoryTests.swift
+++ Tests/CoreDataRepositoryTests/FetchRepositoryTests.swift
@@ -101,7 +101,7 @@ final class FetchRepositoryTests: CoreDataXCTestCase {
 
             })
             .store(in: &cancellables)
-        wait(for: [firstExp], timeout: 10)
+        wait(for: [firstExp], timeout: 20)
         try repositoryContext().performAndWait {
             do {
                 let objectId = try container().persistentStoreCoordinator
@@ -112,6 +112,6 @@ final class FetchRepositoryTests: CoreDataXCTestCase {
                 XCTFail("Failed to update repository: \(error.localizedDescription)")
             }
         }
-        wait(for: [secondExp], timeout: 10)
+        wait(for: [secondExp], timeout: 20)
     }
 }
